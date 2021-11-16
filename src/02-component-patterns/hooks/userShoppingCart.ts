69cb436af96029cb75c50180c447d79dd49c72c0
caarlosdamian <dapecarlos@gmail.com>
2021-11-15 22:45:10 -0600

    Control Props alternative implementation

--- src/02-component-patterns/hooks/userShoppingCart.ts
+++ src/02-component-patterns/hooks/userShoppingCart.ts
@@ -8,31 +8,15 @@ export const useShoppingCard = () => {
 
   const onProductCountChange = ({ count, product }: onChangeArgs) => {
     setShoppingCart((oldShopping) => {
-      const productInCart: ProductInCart = oldShopping[product.id] || {
-        ...product,
-        count: 0,
-      };
-      if (Math.max(productInCart.count + count, 0) > 0) {
-        productInCart.count += count;
-        return {
-          ...oldShopping,
-          [product.id]: productInCart,
-        };
+      if (count === 0) {
+        const { [product.id]: toDelete, ...rest } = oldShopping;
+        console.log(toDelete);
+        return { ...rest };
       }
-      //Borrar el producto
-      const { [product.id]: toDelete, ...rest } = oldShopping;
-      //     console.log(toDelete);
-      return { ...rest };
-
-      //   if (count === 0) {
-      //     const { [product.id]: toDelete, ...rest } = oldShopping;
-      //     console.log(toDelete);
-      //     return { ...rest };
-      //   }
-      //   return {
-      //     ...oldShopping,
-      //     [product.id]: { ...product, count },
-      //   };
+      return {
+        ...oldShopping,
+        [product.id]: { ...product, count },
+      };
     });
   };
 
